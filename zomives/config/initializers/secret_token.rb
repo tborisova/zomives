732feefe45d58089a94f427ad7e2212ea806efae
Tsvetelina Borisova <ts.borisova3@gmail.com>
2013-01-19 08:59:03 +0200

    adding rails app - zomives

---
+++ zomives/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zomives::Application.config.secret_token = '4b1146e8ecc7a281ec70623a109bd60b642e29a5e1762e4c1901f14152d96c5608d32b489a7be77103024313fa5639e1de3eb57d44cb3abadab1461bfd517699'
