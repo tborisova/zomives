600a307f7492b95bf8c8271a02357cfa43cbf484
nicknikolov <nick.nikolov.music@gmail.com>
2013-02-13 10:10:03 +0200

    Initial Commit

---
+++ IPProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IPProject
+//
+//  Created by Nick Nikolov on 13/02/2013.
+//  Copyright (c) 2013 Nick Nikolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
